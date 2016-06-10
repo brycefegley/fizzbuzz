3899b822d676ea89b1b957de8f2226daf779a126
brycefegley <brycefegley@users.noreply.github.com>
2016-06-10 14:13:44 -0700

    Add files via upload

---
+++ fizzbuzz35.py
@@ -0,0 +1,16 @@
+
+def fizzbuzz(n) :
+    n = int(n)
+    if n % 3 == 0 and n % 5 == 0:
+        print("fizzbuzz")
+    elif n % 3 == 0:
+        print("fizz")
+    elif n % 5 == 0:
+        print("buzz")
+    else:
+        print(n)
+        
+n = range(1, 101)
+
+for i in n:
+    fizzbuzz(i)
